62db2220d1e7fe65b6f294bd6a80eed273f3d7a9
Salehziad <salehziad1999@gmail.com>
2022-09-10 14:24:58 +0300

    deploy

---
+++ src/admin/apiAdmin.js
@@ -0,0 +1,119 @@
+export const createCategory = (userId, token, category) => {
+    return fetch(`https://bookland-saleh.herokuapp.com/api/category/create/${userId}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(category)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+
+export const createProduct = (userId, token, product) => {
+    // console.log(userId);
+    return fetch(`https://bookland-saleh.herokuapp.com/api/product/create/${userId}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            // console.log(response);
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const getCategory = categoryId => {
+    return fetch(`https://bookland-saleh.herokuapp.com/api/category/${categoryId}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getCategories = () => {
+    return fetch(`https://bookland-saleh.herokuapp.com/api/categories`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
+
+
+
+/**
+ * to perform crud on product
+ * get all products
+ * get a single product
+ * update single product
+ * delete single product
+ */
+
+export const getProducts = () => {
+    return fetch(`https://bookland-saleh.herokuapp.com/api/products?limit=undefined`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const deleteProduct = (productId, userId, token) => {
+    return fetch(`https://bookland-saleh.herokuapp.com/api/product/${productId}/${userId}`, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getProduct = productId => {
+    return fetch(`https://bookland-saleh.herokuapp.com/api/product/${productId}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const updateProduct = (productId, userId, token, product) => {
+    return fetch(`https://bookland-saleh.herokuapp.com/api/product/${productId}/${userId}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
